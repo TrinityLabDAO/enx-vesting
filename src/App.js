f5e44a1b079fa13ddd64d65750134c3e1e39d292
Pavel <mpakunderscore@gmail.com>
2021-05-26 17:01:04 +0300

    Stable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import {useEffect, useState} from "react";
 import {ABI, ABI2} from "./ABI";
+import {addressContract} from "./contracts";
 
 const Web3 = require("web3");
 
 let token = '0xe0dbb927165c1611a8639e375649b85cc790a702'
-
-let pairs = {
-    '0x91748D8F524c49700FF944480782A77046707fab': '0x6cb1E588b18781783F5Ab4E639Ff1A3f87603E76',
-    '0x40e8ba5285797655f5ca14c9c157536f1d73db20': '0x33134E04D586513B57F558eC70eE6616ec87F77e'
-}
 
 let ethContract
 
@@ -27,7 +23,7 @@ let App = () => {
     const release = async () => {
         if (ethContract) {
             await ethContract.methods.release(token).send({from: address})
-            setReleasableAmount(ethContract)
+            await setReleasableAmount(ethContract)
         }
     }
 
@@ -43,25 +39,32 @@ let App = () => {
             setAddress(accountAddress)
             console.log(accountAddress)
 
-            if (pairs[accountAddress]) {
+            console.log(addressContract)
 
-                console.log(await window.web3.eth.getBalance(pairs[accountAddress]))
+            if (addressContract[accountAddress]) {
 
-                ethContract = new window.web3.eth.Contract(ABI2, pairs[accountAddress])
+                console.log(await window.web3.eth.getBalance(addressContract[accountAddress]))
+
+                let tokenContract = new window.web3.eth.Contract(ABI, token)
+
+                ethContract = new window.web3.eth.Contract(ABI2, addressContract[accountAddress])
 
                 // setContract(ethContract);
 
                 console.log(ethContract)
                 console.log(ethContract.methods)
 
-                setReleasableAmount(ethContract)
-            }
+                setReleasableAmount(ethContract).then()
 
-            if (ethContract.methods.balanceOf)
-                ethContract.methods.balanceOf(token).call((err, val) => {
-                    setContractBalance(val / 1e10)
-                    console.log(val)
-                })
+                console.log(tokenContract)
+                console.log(tokenContract.methods)
+
+                if (tokenContract.methods.balanceOf)
+                    tokenContract.methods.balanceOf(addressContract[accountAddress]).call((err, val) => {
+                        setContractBalance(val / 1e10)
+                        console.log(val)
+                    })
+            }
 
             return true;
         }
@@ -118,9 +121,9 @@ let App = () => {
             <div>Your Address</div>
             <div>{address}</div>
             <div>Contract Address</div>
-            <div>{pairs[address] ? pairs[address] : 'No'}</div>
+            <div>{addressContract[address] ? addressContract[address] : 'No'}</div>
             <div>Contract balance: {contractBalance} ENX</div>
-            <div className={'button'} onClick={release}>Release: {amount}</div>
+            <div className={'button'} onClick={release}>Release: {amount} ENX</div>
         </div>
     );
 }
